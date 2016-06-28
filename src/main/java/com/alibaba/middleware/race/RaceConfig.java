20944a265ed4b3d4c9da53f8788e2d4a5c638b2a
atc_star <atc_star@aliyun.com>
2016-06-29 00:02:43 +0800

    fixed

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -3,11 +3,11 @@ package com.alibaba.middleware.race;
 public class RaceConfig {
 
     //这些是写tair key的前缀
-    public static String prex_tmall = "platformTmall_";
-    public static String prex_taobao = "platformTaobao_";
-    public static String prex_ratio = "ratio_";
+    public static String prex_tmall = "platformTmall_TeamCode_";
+    public static String prex_taobao = "platformTaobao_TeamCode_";
+    public static String prex_ratio = "ratio_TeamCode_";
 
-
+    
     //这些jstorm/rocketMq/tair 的集群配置信息，这些配置信息在正式提交代码前会被公布
     public static String JstormTopologyName = "xxx";
     public static String MetaConsumerGroup = "xxx";
