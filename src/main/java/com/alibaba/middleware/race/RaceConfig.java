1bcc1fb1165f997d4293383cd957fde2e0a82d5f
atc_star <atc_star@aliyun.com>
2016-06-29 08:36:19 +0800

    roll_back

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -9,15 +9,15 @@ public class RaceConfig {
 
     
     //这些jstorm/rocketMq/tair 的集群配置信息，这些配置信息在正式提交代码前会被公布
-    public static String JstormTopologyName = "xxx";
-    public static String MetaConsumerGroup = "xxx";
+    public static String JstormTopologyName = "426291urel";
+    public static String MetaConsumerGroup = "426291urel";
     public static String MqPayTopic = "MiddlewareRaceTestData_Pay";
     public static String MqTmallTradeTopic = "MiddlewareRaceTestData_TMOrder";
     public static String MqTaobaoTradeTopic = "MiddlewareRaceTestData_TBOrder";
-    public static String TairConfigServer = "192.168.1.149:5198";
-    public static String TairSalveConfigServer = "xxx";
-    public static String TairGroup = "group_1";
-    public static Integer TairNamespace = 0;
+    public static String TairConfigServer = "10.101.72.127:5198";
+    public static String TairSalveConfigServer = "10.101.72.128:5198";
+    public static String TairGroup = "group_tianchi";
+    public static Integer TairNamespace = 63828;
     
     //在本地搭建好broker后,记得指定nameServer的地址
    // public static String NamesrvAddr = "192.168.1.149:9876";
