8b8489a9815e9651c4cb43100bf2e4285f026469
atc_star <atc_star@aliyun.com>
2016-07-02 17:36:44 +0800

    fixed-2-7/2

--- src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
@@ -2,6 +2,8 @@ package com.alibaba.middleware.race.jstorm;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.alibaba.jstorm.utils.JStormUtils;
 import com.alibaba.middleware.race.RaceConfig;
 import com.alibaba.middleware.race.SupervisorThread;
 import com.alibaba.middleware.race.jstorm.bolt.PaySortBolt;
